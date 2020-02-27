fd083364bf1f52efd0803b06c3942edfb107b84b
sebasttianvelez <sebasttianvelez@gmail.com>
2020-02-27 10:43:51 -0500

    new platform to deploy in angular

--- MarketArtPlatform/e2e/src/app.e2e-spec.ts
+++ MarketArtPlatform/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('mixDeploy-landing app is running!');
+    expect(page.getTitleText()).toEqual('MarketArtPlatform app is running!');
   });
 
   afterEach(async () => {
--- MarketArtPlatform/src/app/app.component.spec.ts
+++ MarketArtPlatform/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mixDeploy-landing'`, () => {
+  it(`should have as title 'MarketArtPlatform'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mixDeploy-landing');
+    expect(app.title).toEqual('MarketArtPlatform');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mixDeploy-landing app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MarketArtPlatform app is running!');
   });
 });
